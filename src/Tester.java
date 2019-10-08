972e323185a3c3d09643c94d7bf34b1f039833c8
Hadas Ben Mordechai <hadasi135@gmail.com>
2019-10-08 16:24:18 +0300

    update tester

--- src/Tester.java
+++ src/Tester.java
@@ -1,7 +1,20 @@
+import java.util.Arrays;
+
 public class Tester {
 
 	public static void main(String[] args) {
 
+		// Unit tests:
+		AdditionAndMultiplication unitTest = new AdditionAndMultiplication();
+		int[] num1 = {9, 9, 9};
+		int[] num2 = {9, 9, 9};
+
+		// sum
+		System.out.println(Arrays.toString(unitTest.sum(num1, num2)));
+		// multiply
+		System.out.println(Arrays.toString(unitTest.multiply(num1, num2)));
+
+		// Start the server:
 		Server test = new Server();
 		try {
 			test.start();
